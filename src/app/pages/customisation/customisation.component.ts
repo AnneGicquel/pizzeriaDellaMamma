0344dbefe7797e70fd0f6669420c1c4ce84aa470
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-02-20 14:28:58 +0100

    [feat/createPages] Création des pages: - home - category - customisation - basket - summary
    Création du routing dans app-routing-module.ts

---
+++ src/app/pages/customisation/customisation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customisation',
+  templateUrl: './customisation.component.html',
+  styleUrls: ['./customisation.component.css']
+})
+export class CustomisationComponent {
+
+}
