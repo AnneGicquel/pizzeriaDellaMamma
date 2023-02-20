a12c69f47ab2590ec279d7ea7b624581739cb99b
Jiyun <grosjeanjiyun@icloud.com>
2023-02-20 15:32:16 +0100

    category creation button filtre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { CustomisationComponent } from './pages/customisation/customisation.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { TagTypeComponent } from './components/tag-type/tag-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoryComponent,
+    CustomisationComponent,
+    BasketComponent,
+    SummaryComponent,
+    TagTypeComponent
   ],
   imports: [
     BrowserModule,
