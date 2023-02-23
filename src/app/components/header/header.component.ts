b8f593615f7adbe617e4e8e1d8bde81d7eb2ba21
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-02-23 12:08:58 +0100

    recupération du nombre d'article dans le panier pour affichage icone

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BasketServiceService, IBasketProduct } from 'src/app/services/basketService/basket-service.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  basket : IBasketProduct[] = [];
+  quantity?: number = 0;
+  
+
+  constructor (private basketService : BasketServiceService) {}
+
+  ngOnInit() {
+    this.getNumberBasket();
+  }
+
+
+  getNumberBasket() {
+    const basket = this.basketService.getBasket();
+    const quantity = basket.length;
+    this.quantity = quantity;
+    
+  
+  }
 
 }
